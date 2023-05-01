0bedae489a2932179d18f81a3548e25d8d6895b3
Theofilusarifin <theofilusarifin@gmail.com>
2023-05-01 07:42:12 +0700

    Add view

--- app/src/main/java/com/example/healthcare_umc_160420046/model/model.kt
+++ app/src/main/java/com/example/healthcare_umc_160420046/model/model.kt
@@ -1,2 +1,20 @@
 package com.example.healthcare_umc_160420046.model
 
+data class Doctor(
+    val id:String?,
+    val name:String?,
+    val type:String?,
+    val rating:Int?,
+    val description:String?,
+    val dob:String?,
+    val age:Int?,
+    val gender:String?,
+
+    )
+
+data class Medicine(
+    val id:String?,
+    val name:String?,
+    val stock:String?,
+    val price:String?,
+    )
--- app/src/main/java/com/example/healthcare_umc_160420046/util/util.kt
+++ app/src/main/java/com/example/healthcare_umc_160420046/util/util.kt
@@ -1,2 +1,24 @@
 package com.example.healthcare_umc_160420046.util
 
+import android.view.View
+import android.widget.ImageView
+import android.widget.ProgressBar
+import com.example.healthcare_umc_160420046.R
+import com.squareup.picasso.Callback
+import com.squareup.picasso.Picasso
+fun ImageView.loadImage(url:String, progressBar: ProgressBar){
+    Picasso.get()
+        .load(url)
+        .resize(400, 400)
+        .centerCrop()
+        .error(R.drawable.ic_baseline_error_24)
+        .into(this, object: Callback {
+            override fun onSuccess() {
+                progressBar.visibility = View.GONE
+            }
+            override fun onError(e: Exception?) {
+            }
+        })
+
+
+}
