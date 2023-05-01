1bf927eac208c4b51aa98dd9b0ac4bb2640a0586
Theofilusarifin <theofilusarifin@gmail.com>
2023-05-01 12:34:38 +0700

    Login Register Done

--- app/src/main/java/com/example/healthcare_umc_160420046/view/LoginFragment.kt
+++ app/src/main/java/com/example/healthcare_umc_160420046/view/LoginFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.Navigation
 import com.example.healthcare_umc_160420046.R
 
 class LoginFragment : Fragment() {
@@ -15,5 +18,18 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btnLogin = view.findViewById<Button>(R.id.btnLogin)
+        val txtUsername = view.findViewById<TextView>(R.id.txtUsername)
+
+        btnLogin.setOnClickListener {
+            val username = txtUsername.text.toString()
+            val action = LoginFragmentDirections.actionLoginToDoctorList(username)
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 }
--- app/src/main/java/com/example/healthcare_umc_160420046/view/RegisterFragment.kt
+++ app/src/main/java/com/example/healthcare_umc_160420046/view/RegisterFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.Navigation
 import com.example.healthcare_umc_160420046.R
 
 class RegisterFragment : Fragment() {
@@ -15,5 +18,24 @@ class RegisterFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val txtLoginHere = view.findViewById<TextView>(R.id.txtLoginHere)
+        val btnRegister = view.findViewById<Button>(R.id.btnRegister)
+        val txtNewUsername = view.findViewById<TextView>(R.id.txtNewUsername)
+
+        txtLoginHere.setOnClickListener {
+            val action = RegisterFragmentDirections.actionRegToLog()
+            Navigation.findNavController(it).navigate(action)
+        }
+
+        btnRegister.setOnClickListener {
+            val username = txtNewUsername.text.toString()
+            val action = RegisterFragmentDirections.actionRegisterToDoctorList(username)
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 }
