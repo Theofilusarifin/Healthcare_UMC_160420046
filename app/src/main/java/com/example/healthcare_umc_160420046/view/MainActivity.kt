bcc08199390d71a1d29d5e5e71ce906a98fb6361
Theofilusarifin <theofilusarifin@gmail.com>
2023-05-01 11:48:46 +0700

    Login Register

--- app/src/main/java/com/example/healthcare_umc_160420046/view/MainActivity.kt
+++ app/src/main/java/com/example/healthcare_umc_160420046/view/MainActivity.kt
@@ -1,12 +1,64 @@
 package com.example.healthcare_umc_160420046.view
 
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.example.healthcare_umc_160420046.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
+    private fun showNavigation(){
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.visibility = View.VISIBLE
+
+        val navView = findViewById<NavigationView>(R.id.navView)
+        navView.visibility = View.VISIBLE
+    }
+
+    private fun hideNavigation(){
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.visibility = View.GONE
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+        val navView = findViewById<NavigationView>(R.id.navView)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout)
+        NavigationUI.setupWithNavController(navView,navController)
+
+        bottomNavigationView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener{_, destination, _ ->
+            when(destination.id){
+                R.id.loginFragment -> hideNavigation()
+                R.id.registerFragment -> hideNavigation()
+                else -> showNavigation()
+            }
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+        return NavigationUI.navigateUp(navController,drawerLayout) || super.onSupportNavigateUp()
     }
 }
