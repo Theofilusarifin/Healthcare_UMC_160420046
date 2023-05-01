b87e19c831660392d69a68b1303fc269b0456268
Theofilusarifin <theofilusarifin@gmail.com>
2023-05-01 14:32:56 +0700

    View List Fragment

--- app/src/main/java/com/example/healthcare_umc_160420046/view/DoctorListFragment.kt
+++ app/src/main/java/com/example/healthcare_umc_160420046/view/DoctorListFragment.kt
@@ -5,9 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.healthcare_umc_160420046.R
+import com.example.healthcare_umc_160420046.viewmodel.DoctorListViewModel
 
 class DoctorListFragment : Fragment() {
+    private lateinit var viewModel: DoctorListViewModel
+    private val doctorListAdapter = DoctorListAdapter(arrayListOf())
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,6 +25,65 @@ class DoctorListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_doctor_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this)[DoctorListViewModel::class.java]
+        viewModel.refresh()
+
+        val recView = view.findViewById<RecyclerView>(R.id.recView)
+        val refreshLayout = view.findViewById<SwipeRefreshLayout>(R.id.refreshLayout)
+        val txtError = view.findViewById<TextView>(R.id.txtError)
+        val progressLoad = view.findViewById<ProgressBar>(R.id.progressLoad)
+
+        recView.layoutManager = LinearLayoutManager(context)
+        recView.adapter = doctorListAdapter
+
+        refreshLayout.setOnRefreshListener {
+            recView.visibility = View.GONE
+            txtError.visibility = View.GONE
+            progressLoad.visibility  = View.VISIBLE
+            viewModel.refresh()
+            refreshLayout.isRefreshing = false
+        }
+
+        observeViewModel()
+    }
+
+    private fun observeViewModel(){
+        viewModel.doctorLD.observe(viewLifecycleOwner, Observer {
+            doctorListAdapter.updateDoctorList(it)
+        })
+
+        viewModel.loadingErrorLD.observe(viewLifecycleOwner, Observer {
+            val txtError = view?.findViewById<TextView>(R.id.txtError)
+            if (txtError != null) {
+                txtError.visibility = if(it) View.VISIBLE else View.GONE
+            }
+        })
+
+        viewModel.loadingLD.observe(viewLifecycleOwner, Observer {
+            val progressLoad = view?.findViewById<ProgressBar>(R.id.progressLoad)
+            val recView = view?.findViewById<RecyclerView>(R.id.recView)
+
+            if(it){
+                if (progressLoad != null) {
+                    progressLoad.visibility = View.VISIBLE
+                }
+                if (recView != null) {
+                    recView.visibility = View.GONE
+                }
+            } else{
+                if (progressLoad != null) {
+                    progressLoad.visibility = View.GONE
+                }
+                if (recView != null) {
+                    recView.visibility = View.VISIBLE
+                }
+            }
+        })
     }
 
 }
--- app/src/main/java/com/example/healthcare_umc_160420046/view/MedicineListFragment.kt
+++ app/src/main/java/com/example/healthcare_umc_160420046/view/MedicineListFragment.kt
@@ -5,9 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.healthcare_umc_160420046.R
+import com.example.healthcare_umc_160420046.viewmodel.MedicineListViewModel
 
 class MedicineListFragment : Fragment() {
+    private lateinit var viewModel: MedicineListViewModel
+    private val medicineListAdapter = MedicineListAdapter(arrayListOf())
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -15,4 +26,64 @@ class MedicineListFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_medicine_list, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this)[MedicineListViewModel::class.java]
+        viewModel.refresh()
+
+        val recView = view.findViewById<RecyclerView>(R.id.recView)
+        val refreshLayout = view.findViewById<SwipeRefreshLayout>(R.id.refreshLayout)
+        val txtError = view.findViewById<TextView>(R.id.txtError)
+        val progressLoad = view.findViewById<ProgressBar>(R.id.progressLoad)
+
+        recView.layoutManager = LinearLayoutManager(context)
+        recView.adapter = medicineListAdapter
+
+        refreshLayout.setOnRefreshListener {
+            recView.visibility = View.GONE
+            txtError.visibility = View.GONE
+            progressLoad.visibility  = View.VISIBLE
+            viewModel.refresh()
+            refreshLayout.isRefreshing = false
+        }
+
+        observeViewModel()
+    }
+
+    private fun observeViewModel(){
+        viewModel.medicineLD.observe(viewLifecycleOwner, Observer {
+            medicineListAdapter.updateDoctorList(it)
+        })
+
+        viewModel.loadingErrorLD.observe(viewLifecycleOwner, Observer {
+            val txtError = view?.findViewById<TextView>(R.id.txtError)
+            if (txtError != null) {
+                txtError.visibility = if(it) View.VISIBLE else View.GONE
+            }
+        })
+
+        viewModel.loadingLD.observe(viewLifecycleOwner, Observer {
+            val progressLoad = view?.findViewById<ProgressBar>(R.id.progressLoad)
+            val recView = view?.findViewById<RecyclerView>(R.id.recView)
+
+            if(it){
+                if (progressLoad != null) {
+                    progressLoad.visibility = View.VISIBLE
+                }
+                if (recView != null) {
+                    recView.visibility = View.GONE
+                }
+            } else{
+                if (progressLoad != null) {
+                    progressLoad.visibility = View.GONE
+                }
+                if (recView != null) {
+                    recView.visibility = View.VISIBLE
+                }
+            }
+        })
+    }
+
 }
